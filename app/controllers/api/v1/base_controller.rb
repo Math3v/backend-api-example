80dd8024365bc54ca21da907ed1d84a655658895
Matej Minarik <mato.minarik@azet.sk>
2017-05-27 16:20:05 +0200

    Add authentication

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::BaseController < ApplicationController
+
+  before_action :authenticate
+
+  private
+
+    def authenticate
+      key = request.headers['HTTP_API_KEY']
+      unauthorized if Token.where(value: key).empty?
+    end
+
+    def unauthorized
+      render json: 'Unauthorized', status: :unauthorized
+    end
 end
